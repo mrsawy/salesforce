56e3efb7da93300c394091d0468e6dc043c95bf2
mrsawy <mrsawy@gmail.com>
2024-04-29 09:51:20 +0300

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,8 +7,11 @@ const SECRET_KEY = process.env.secret_key;
 module.exports = {
   login: async (req, res) => {
     let { email, password } = req.body;
-    console.log(email , password)
+    console.log(email, password);
     const admin = await Admin.findOne({ email });
+
+    const allAdmin = await Admin.find({});
+    console.log({ allAdmin });
     if (!admin) {
       return res.status(401).json({ error: "Invalid Email" });
     }
